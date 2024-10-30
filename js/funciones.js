47b22c326d8f51df108cd0d8969907a226a2fa41
Giorgio <guerreroanthony9707@gmail.com>
2024-10-30 12:10:29 +0100

    Falta anhadir a tabla y boton borrar

--- js/funciones.js
+++ js/funciones.js
@@ -1,16 +1,43 @@
-var productos = [
+let productos = [
   ["Chapata", 0.65],
   ["Baguette", 0.55],
   ["Pistola", 0.45],
 ];
 
-function mostrarLista() {
-  var productoss = document.getElementById("producto");
+function mostrarProductos() {
+  let productoss = document.getElementById("producto");
   for (let i = 0; i < productos.length; i++) {
-    var producto = new Option(productos[i][0], productos[i][0]);
+    let producto = new Option(productos[i][0], productos[i][0]);
     productoss.options.add(producto);
-    console.log(productos[i]);
   }
 }
 
-function cargarProductos() {}
+function cargarPrecio() {
+ let listaProductos = document.getElementById("producto");
+ let listaPrecios = document.getElementById("precio");
+ let productSelect = listaProductos.options[listaProductos.selectedIndex].value;
+
+while (listaPrecios.options.length) {
+  listaPrecios.remove(0)
+}
+
+for (let i = 0; i < productos.length; i++) {
+  if(productSelect == productos[i][0]){
+    let producto = new Option(productos[i][1]+' €',parseFloat(productos[i][1]))
+    listaPrecios.options.add(producto)
+    console.log(producto)
+  }
+}
+}
+
+function addProducto(){
+  let cantidad = document.getElementById("cantidad").value
+  let listaPrecios = document.getElementById('precio');
+  let precioSelect = listaPrecios.options[listaPrecios.selectedIndex].value;
+  total = cantidad*precioSelect
+  console.log(total.toFixed(2))
+ 
+
+
+
+}
